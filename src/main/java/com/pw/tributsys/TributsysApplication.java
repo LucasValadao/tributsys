14a3627a7fb80b185c97d6a775f34917fbdebd48
Lucas Valadao <lukasfv10@gmail.com>
2022-05-31 16:14:12 -0300

    [add]Model e conexao com postgresql

---
+++ src/main/java/com/pw/tributsys/TributsysApplication.java
@@ -0,0 +1,13 @@
+package com.pw.tributsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TributsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TributsysApplication.class, args);
+	}
+
+}
